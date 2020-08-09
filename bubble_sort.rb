59717b760833c6fbcd97badfe15c150fae5677b0
xtrator <65496317+xtrator@users.noreply.github.com>
2020-08-09 10:18:35 -0500

    Added guiding comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,10 @@
 def bubble_sort(array)
-    array.each_with_index do |num, i|
-        return array if i == (array.length-1)
-        if array[i] > array[i+1]
-            array[i] = array[i+1]
-            array[i+1] = num
-            bubble_sort(array)
+    array.each_with_index do |num, i|   
+        return array if i == (array.length-1)  #end point of recurssion, returns array when index reaches last
+        if array[i] > array[i+1]    #if the current number is more than the next number
+            array[i] = array[i+1]       #replace current number with next number
+            array[i+1] = num            #replace next number with the current number
+            bubble_sort(array)          #call the same function with the modified array to start over from index 0
         end
     end
 end 
